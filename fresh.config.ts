102f4e750fbba1606e20664842b73c653143cc0b
Bryan Markham <muzik702@gmail.com>
2025-02-18 01:52:23 -0500

    Correcting build

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
+import twindConfig from "./tailwind.config.ts";
 
 export default defineConfig({
-  plugins: [tailwind()],
+  plugins: [
+    tailwind(twindConfig),
+  ],
 });
