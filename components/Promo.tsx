5278e6afe3310ae2536a77e6f39876c064b497c8
Bryan Markham <muzik702@gmail.com>
2025-02-20 09:56:27 -0500

    Correcting promo

--- components/Promo.tsx
+++ components/Promo.tsx
@@ -1,7 +1,7 @@
 export default function Promo() {
     return (
       <div class="bg-calchamber-green-light py-2 mb-8 text-center text-calchamber-green-dark">
-        Preferred or higher level members save up to <strong>20%</strong> year-round on products and training.
+        Preferred and higher level members save up to <strong>20%</strong> year-round on products and training.
       </div>
     );
   }
